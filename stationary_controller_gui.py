c206cd1bb0fd092a953631a0dce7046a6e5f2bd8
Ali K <karimiafsharali@gmail.com>
2022-02-26 19:42:26 -0600

    refactor GUI class methods

--- stationary_controller_gui.py
+++ stationary_controller_gui.py
@@ -11,16 +11,26 @@ class GUI(ttk.Frame):
         self.grid()
 
         # create an instance of style object
-        style = ttk.Style()
+        self.style = ttk.Style()
 
+        self.create_panes()
+
+        self.create_control_buttons()
+
+        self.configure_styles()
+
+    def create_panes(self):
         # Create Panes
         self.display_pane = ttk.Frame(self, style="button.TFrame", padding=10)
         self.control_pane = ttk.Frame(self, style="display.TFrame", padding=10)
+        self.command_pane = ttk.Frame(self, style="command.TFrame", padding=10)
 
         # Grid Panes
         self.display_pane.grid(row=0, column=0)
         self.control_pane.grid(row=1, column=0)
+        self.command_pane.grid(row=2, column=0)
 
+    def create_control_buttons(self):
         # Create Control Buttons
         self.btn_move_forward = ttk.Button(
             self.control_pane,
@@ -49,25 +59,23 @@ class GUI(ttk.Frame):
         self.btn_rotate_right.grid(row=1, column=2)
         self.btn_rotate_left.grid(row=1, column=0)
 
+    def configure_styles(self):
         # Configure styles
-        style.configure(
+        self.style.configure(
             "control.TButton",
             padding=0,
             background="#1cc",
             width=4,
             font=(None, 25),
         )
-
-        style.configure(
+        self.style.configure(
             "Tlabel",
         )
-
-        style.configure(
+        self.style.configure(
             "button.TFrame",
             background="#fcc200",
         )
-
-        style.configure(
+        self.style.configure(
             "display.TFrame",
             background="maroon",
         )
