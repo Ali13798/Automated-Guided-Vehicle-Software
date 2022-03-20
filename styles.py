a12eba33cd3d1344cf778299643ca791778d74e5
Ali K <karimiafsharali@gmail.com>
2022-03-20 14:38:20 -0500

    add styles configurations for GUI

---
+++ styles.py
@@ -0,0 +1,46 @@
+FONT_SIZE = 20
+PADDING = 5
+
+
+class AgvStyles:
+    @staticmethod
+    def config_styles(style):
+        style.configure(
+            "TButton",
+            background="#fcc200",
+            font=(None, FONT_SIZE),
+            padding=PADDING,
+        )
+        style.configure(
+            "control.TButton",
+            width=3,
+        )
+
+        # Configure styles - Labels
+        style.configure(
+            "TLabel",
+            font=(None, FONT_SIZE),
+            background="#5e0009",
+            foreground="white",
+        )
+
+        # Configure styles - Frames
+        style.configure(
+            "TFrame",
+            background="#5e0009",
+        )
+        style.configure(
+            "display.TFrame",
+        )
+        style.configure(
+            "welcome.TFrame",
+        )
+
+        # Configure styles - Textbox
+        style.configure(
+            "TEntry",
+            font=(None, FONT_SIZE),
+            foreground="green",
+        )
+
+        return style
