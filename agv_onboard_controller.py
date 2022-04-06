276b1393b645c73a31871956a649ad9a0782e1a4
Ali K <karimiafsharali@gmail.com>
2022-04-06 16:46:22 -0500

    add freq calculator based on linear velocity

---
+++ agv_onboard_controller.py
@@ -0,0 +1,33 @@
+import math
+
+# unit: inch
+WHEEL_DIAM = 5
+
+# unit: degrees
+STEP_ANGLE = 0.9
+
+
+def calc_pulse_freq(velocity: float = 2.25):
+
+    # unit: in/s
+    velocity_inps = velocity * 12
+
+    # unit: radians/s
+    omega_rps = velocity_inps / (WHEEL_DIAM / 2)
+
+    # unit: degrees/s
+    omega_dps = omega_rps * 180 / math.pi
+
+    # unit: 1/s (Hz)
+    pulse_freq = omega_dps / STEP_ANGLE
+
+    return pulse_freq
+
+
+def main():
+    freq = calc_pulse_freq()
+    print(freq)
+
+
+if __name__ == "__main__":
+    main()
