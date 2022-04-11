ad1d5719438a8faeaa237a005424ebc49a80e3f3
Ali K <karimiafsharali@gmail.com>
2022-04-11 15:32:45 -0500

    connection bw client and server w handshake

--- client.py
+++ client.py
@@ -1,9 +1,13 @@
 import atexit
 import socket
 
-FORMAT = "utf-8"
-HEADERSIZE = 16
-DISCONNECT_MESSAGE = "!DISCONNECT"
+from config import read_config
+
+conf = read_config()
+FORMAT = conf.socket_encoding_format
+HEADERSIZE = conf.socket_message_header_size
+DISCONNECT_MESSAGE = conf.socket_disconnect_message
+HANDSHAKE = conf.socket_establish_connection_message
 
 
 class Client:
@@ -12,21 +16,41 @@ class Client:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((server_ip, port))
 
+        self.connected = self.establish_connection()
+
         atexit.register(self.cleanup)
 
-    def send_message_to_server(self, msg: str) -> None:
+    def send_message(self, msg: str) -> None:
         message = msg.encode(FORMAT)
-        msg_length = len(message)
 
+        msg_length = len(message)
         send_length = f"{msg_length:<{HEADERSIZE}}"
+
         self.client.send(send_length.encode(FORMAT))
         self.client.send(message)
-        print(f"Sending a message of length {msg_length}.")
 
-    # @atexit.register
+        print(f"[SENDING] length {msg_length:>3}: {msg}")
+
+    def read_message(self) -> str | None:
+        msg_length = self.client.recv(HEADERSIZE).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = self.client.recv(msg_length).decode(FORMAT)
+            return msg
+
+    def establish_connection(self) -> bool:
+        msg = self.read_message()
+        if msg == HANDSHAKE:
+            self.send_message(HANDSHAKE)
+            print("[CONN SUCCESS] connected to the Server.")
+            return True
+
+        print("[CONN ERROR] Client connection failed.")
+        return False
+
     def cleanup(self):
-        print("Program ended. Starting cleanup...")
-        self.send_message_to_server(DISCONNECT_MESSAGE)
+        print("[TERMINATE] Program ended. Starting cleanup...")
+        self.send_message(DISCONNECT_MESSAGE)
 
 
 def main():
@@ -37,7 +61,7 @@ def main():
 
     while True:
         text = input("Enter message: ")
-        client.send_message_to_server(text)
+        client.send_message(text)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-import atexit
 import socket
 import threading
 
-FORMAT = "utf-8"
-DISCONNECT_MESSAGE = "!DISCONNECT"
-HEADERSIZE = 16
+from config import read_config
+
+conf = read_config()
+FORMAT = conf.socket_encoding_format
+HEADERSIZE = conf.socket_message_header_size
+DISCONNECT_MESSAGE = conf.socket_disconnect_message
+HANDSHAKE = conf.socket_establish_connection_message
 
 
 class Server:
@@ -14,32 +17,61 @@ class Server:
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((ip, port))
 
-    def handle_client(self, client: socket.socket, addr) -> None:
-        print(f"[NEW CONNECTION] {addr} connected.")
-
-        connected = True
-        while connected:
-            msg_length = client.recv(HEADERSIZE).decode(FORMAT)
-            if msg_length:
-                msg_length = int(msg_length)
-                msg = client.recv(msg_length).decode(FORMAT)
-                print(f"[{addr}] {msg}")
-
-                if msg == DISCONNECT_MESSAGE:
-                    connected = False
-
-        # If not connected close the socket
-        client.close()
-
     def start(self):
         self.server.listen()
         print(f"[LISTENNING] Server is listenning on {self.SERVER}")
-        client, addr = self.server.accept()
-        thread = threading.Thread(
-            target=self.handle_client, args=(client, addr)
-        )
+
+        self.client, addr = self.server.accept()
+
+        self.connected = self.establish_connection()
+
+        thread = threading.Thread(target=self.handle_client, args=(addr,))
         thread.start()
-        # print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+    def send_message(self, msg: str) -> None:
+        message = msg.encode(FORMAT)
+
+        msg_length = len(message)
+        send_length = f"{msg_length:<{HEADERSIZE}}"
+
+        self.client.send(send_length.encode(FORMAT))
+        self.client.send(message)
+
+        print(f"[SENDING] length {msg_length:>3}: {msg}")
+
+    def read_message(self) -> str | None:
+        msg_length = self.client.recv(HEADERSIZE).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = self.client.recv(msg_length).decode(FORMAT)
+            return msg
+
+    def establish_connection(self):
+        self.send_message(HANDSHAKE)
+        msg = self.read_message()
+
+        if msg == HANDSHAKE:
+            return True
+        print("[CONN ERROR] Client connection failed.")
+        return False
+
+    def handle_client(self, addr: str) -> None:
+        print(f"[NEW CONNECTION] {addr} connected.")
+
+        # connected = True
+        while self.connected:
+            msg = self.read_message()
+            if msg is None:
+                continue
+
+            print(f"[{addr}] {msg}")
+
+            if msg == DISCONNECT_MESSAGE:
+                self.connected = False
+
+        # If not connected close the socket
+        print("[Terminate] closing connection...")
+        self.client.close()
 
 
 def main():
