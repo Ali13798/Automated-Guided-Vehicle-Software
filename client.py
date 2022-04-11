c57ffa5a35cb1da634285df427d84aac90fcccb0
Ali K <karimiafsharali@gmail.com>
2022-04-11 12:43:39 -0500

    create Client class

--- client.py
+++ client.py
@@ -1,27 +1,44 @@
+import atexit
 import socket
 
-PORT = 1234
-host_name = socket.gethostname()
-IP = socket.gethostbyname(host_name)
 FORMAT = "utf-8"
 HEADERSIZE = 16
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((IP, PORT))
+class Client:
+    def __init__(self, server_ip: str, port: str) -> None:
+        self.CLIENT = server_ip
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((server_ip, port))
+
+        atexit.register(self.cleanup)
+
+    def send_message_to_server(self, msg: str) -> None:
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+
+        send_length = f"{msg_length:<{HEADERSIZE}}"
+        self.client.send(send_length.encode(FORMAT))
+        self.client.send(message)
+        print(f"Sending a message of length {msg_length}.")
+
+    # @atexit.register
+    def cleanup(self):
+        print("Program ended. Starting cleanup...")
+        self.send_message_to_server(DISCONNECT_MESSAGE)
 
 
-def send_message(msg: str) -> None:
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
+def main():
+    PORT = 1234
+    CLIENT = socket.gethostbyname(socket.gethostname())
 
-    send_length = f"{msg_length:<{HEADERSIZE}}".encode(FORMAT)
-    client.send(send_length)
-    client.send(message)
-    print(send_length)
+    client = Client(server_ip=CLIENT, port=PORT)
+
+    while True:
+        text = input("Enter message: ")
+        client.send_message_to_server(text)
 
 
-while True:
-    text = input("Enter message: ")
-    send_message(text)
+if __name__ == "__main__":
+    main()
