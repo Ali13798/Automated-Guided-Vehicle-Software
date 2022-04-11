36a3e90189972416cdb1af6d69ced0be4b96cc9e
Ali K <karimiafsharali@gmail.com>
2022-04-10 22:57:42 -0500

    method to calculate pulses needed for desired dist

--- agv_tools.py
+++ agv_tools.py
@@ -11,7 +11,7 @@ STEP_ANGLE = 360 / STEP_DRIVER_FREQ
 
 
 class AgvTools:
-    def calc_pulse_freq(velocity: float = 2.25):
+    def calc_pulse_freq(velocity: float = 2.25) -> int:
 
         # unit: in/s
         velocity_inps = velocity * 12
@@ -25,4 +25,13 @@ class AgvTools:
         # unit: 1/s (Hz)
         pulse_freq = omega_dps / STEP_ANGLE
 
-        return pulse_freq
+        return int(round(pulse_freq, 0))
+
+    def calc_pulse_num(inches: float) -> float:
+        # radians
+        angle = inches / (WHEEL_DIAM / 2)
+
+        # degrees
+        angle = angle * 180 / math.pi
+
+        return angle / STEP_ANGLE
