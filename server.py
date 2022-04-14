93048d9fa801285a4f3c921ebc8c17ca414838cc
Ali K <karimiafsharali@gmail.com>
2022-04-13 22:30:12 -0500

    start the server as Controller initializes

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
+from time import sleep
 
 from agv_socket import AgvSocket
+from onboard_controller import Controller
 
 
 def main():
@@ -9,7 +11,16 @@ def main():
 
     server = AgvSocket(ip=SERVER, port=PORT, isServer=True)
 
-    server.start_server()
+    ctrl = Controller(server)
+
+    # print("STARTING TO WAIT")
+
+    # for i in range(5):
+    #     print("SECOND IS: ", i)
+    #     sleep(5)
+
+    # print(ctrl.shared_list)
+    # server.start_server()
 
 
 if __name__ == "__main__":
