2efeddb79cdb620b77721ec0637e2ff22470cf93
Ali K <karimiafsharali@gmail.com>
2022-04-13 21:19:56 -0500

    add comments and documentation

--- agv_socket.py
+++ agv_socket.py
@@ -7,6 +7,15 @@ from config import read_config
 
 class AgvSocket:
     def __init__(self, ip: str, port: int, isServer: bool = False) -> None:
+        """Server or client socket for transmitting messages between the
+        AGV and Controller.
+
+        Args:
+            ip (str): The IP address of the server/client
+            port (int): The Port number of the server/client
+            isServer (bool, optional): Dictates whether this object is
+                the server or client. Defaults to False.
+        """
 
         # Configuration values
         self.config = read_config()
@@ -32,19 +41,26 @@ class AgvSocket:
             self.connected = self.establish_connection()
 
     def establish_connection(self) -> bool:
+        """Exchanges a handshake message between the server and client.
+
+        Returns:
+            bool: True if the handshake was successful, or False otherwise.
+        """
+
+        # If the current object is a server.
         if self.isServer:
             self.send_message(self.HANDSHAKE)
 
-            msg = self.read_message()
-            if msg == self.HANDSHAKE:
+            response = self.read_message()
+            if response == self.HANDSHAKE:
                 return True
 
             print("[CONN ERROR] Client connection failed.")
             return False
 
-        # if this is not the server
-        msg = self.read_message()
-        if msg == self.HANDSHAKE:
+        # If the current object is a client.
+        response = self.read_message()
+        if response == self.HANDSHAKE:
             self.send_message(self.HANDSHAKE)
             print("[CONN SUCCESS] connected to the Server.")
             return True
@@ -53,12 +69,23 @@ class AgvSocket:
         return False
 
     def read_message(self) -> str | None:
+        """Receives a message from the connected socket using a
+        header containing the length of the proceedign message.
+
+        Returns:
+            str | None: The received message, if not the initial connection.
+        """
+
         try:
             msg_length = self.client.recv(self.HEADERSIZE).decode(self.FORMAT)
+
+            # If the msg_length exists:
             if msg_length:
                 msg_length = int(msg_length)
                 msg = self.client.recv(msg_length).decode(self.FORMAT)
                 return msg
+
+            # If the msg_length does not exist.
             return
         except ConnectionResetError:
             print("Connection forcibly closed.")
@@ -66,17 +93,24 @@ class AgvSocket:
             return
 
     def send_message(self, msg: str) -> None:
-        message = msg.encode(self.FORMAT)
+        """Sends the message to the connected socket using the encoding format
+        specified.
 
-        msg_length = len(message)
+        Args:
+            msg (str): The message to be transmitted.
+        """
+
+        msg_length = len(msg.encode(self.FORMAT))
         send_length = f"{msg_length:<{self.HEADERSIZE}}"
 
+        print(f"[SENDING] length ({msg_length:>3}): {msg}")
+
         self.client.send(send_length.encode(self.FORMAT))
-        self.client.send(message)
+        self.client.send(msg.encode(self.FORMAT))
 
-        print(f"[SENDING] length {msg_length:>3}: {msg}")
+    def start_server(self) -> None:
+        """Starts the server and waits for client connection."""
 
-    def start_server(self):
         if not self.isServer:
             return
 
@@ -92,11 +126,17 @@ class AgvSocket:
         thread = threading.Thread(target=self.handle_client, args=(addr,))
         thread.start()
 
-    def handle_client(self, addr: str) -> None:
+    def handle_client(self, addr: tuple[str, int]) -> None:
+        """While the client is connected, receives messages from the client.
+
+        Args:
+            addr (tuple[str, int]): (IP address, Port number) of the client.
+        """
+
         if not self.isServer:
             return
 
-        print(f"[NEW CONNECTION] {addr} connected.")
+        print(f"[NEW CONNECTION] {type(addr)} connected.")
 
         while self.connected:
             msg = self.read_message()
@@ -110,11 +150,14 @@ class AgvSocket:
 
             self.send_message(self.MESSAGE_RECEIVED)
 
-        # If not connected close the socket
-        print("[Terminate] closing connection...")
+        # If not connected close the socket.
+        print("[TERMINATE] closing connection...")
         self.client.close()
 
-    def cleanup_server(self):
+    def cleanup_server(self) -> None:
+        """Sends disconnect message to server if client is
+        unexpectedly closed."""
+
         print("[TERMINATE] Program ended. Starting cleanup...")
         self.send_message(self.DISCONNECT_MESSAGE)
 
