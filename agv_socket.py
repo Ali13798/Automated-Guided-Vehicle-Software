8cba18cfa277eaa2f88184e465a2ddca9cd5e2e9
Ali K <karimiafsharali@gmail.com>
2022-04-13 20:46:03 -0500

    refactor controller methods to class, rm from here

--- agv_socket.py
+++ agv_socket.py
@@ -2,9 +2,7 @@ import atexit
 import socket
 import threading
 
-from agv_command import AgvCommand
 from config import read_config
-from instructions import Instruction
 
 
 class AgvSocket:
@@ -21,10 +19,6 @@ class AgvSocket:
         self.ip = ip
         self.port = port
         self.isServer = isServer
-
-        self.mutex = threading.Lock()
-        self.instructions: list[Instruction] = []
-        self.valid_commands = [cmd.value for cmd in AgvCommand]
 
         if isServer:
             self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -124,60 +118,6 @@ class AgvSocket:
     def cleanup_server(self):
         print("[TERMINATE] Program ended. Starting cleanup...")
         self.send_message(self.DISCONNECT_MESSAGE)
-
-    def add_instruction(self, inst: Instruction) -> None:
-        self.mutex.acquire()
-        self.instructions.append(inst)
-        self.mutex.release()
-
-    def consume_instruction(self) -> Instruction:
-        self.mutex.acquire()
-        inst = self.instructions.pop(0)
-        self.mutex.release()
-        return inst
-
-    def parse_message(self, msg: str) -> Instruction | None:
-        msg = msg.split()
-        msg = [n.upper() for n in msg]
-        # print(msg)
-        if len(msg) != 2:
-            print(f"[INVALID COMMAND] Expected 2 words, but got {len(msg)}.")
-            self.send_message(AgvCommand.invalid.value)
-            return
-
-        if msg[0] not in self.valid_commands:
-            print(f'[INVALID COMMAND] command "{msg[0]}" is not valid.')
-            self.send_message(AgvCommand.invalid.value)
-            return
-
-        try:
-            int(msg[1])
-        except ValueError:
-            print(
-                f'[INVALID COMMAND] expected a number as second term, but got "{msg[1]}."'
-            )
-            self.send_message(AgvCommand.invalid.value)
-            return
-
-        # TODO: FInd the actual command corresponding with the msg
-        inst = Instruction(command=msg[0], value=msg[1])
-        self.send_message(AgvCommand.valid.value)
-
-        return inst
-
-    def instruction_handler(self):
-        while True:
-            self.mutex.acquire()
-            n = len(self.instructions)
-            if n > 0:
-                # process instruction then pop
-                pass
-
-            self.mutex.release()
-
-    def emergency_stop(self):
-        # stop everything, then clear instruction list.
-        return
 
 
 def main():
--- onboard_controller.py
+++ onboard_controller.py
@@ -1,6 +1,76 @@
 import math
+import threading
 
+from agv_command import AgvCommand
 from agv_tools import AgvTools as agv_tools
+from instructions import Instruction
+from mode import Mode
+
+
+class Controller:
+    def __init__(self) -> None:
+        self.mode = Mode.Unselected
+
+        self.mutex = threading.Lock()
+        self.instructions: list[Instruction] = []
+        self.valid_commands = [cmd.value for cmd in AgvCommand]
+
+    def establish_connection(self):
+        pass
+
+    def add_instruction(self, inst: Instruction) -> None:
+        self.mutex.acquire()
+        self.instructions.append(inst)
+        self.mutex.release()
+
+    def consume_instruction(self) -> Instruction:
+        self.mutex.acquire()
+        inst = self.instructions.pop(0)
+        self.mutex.release()
+        return inst
+
+    def parse_message(self, msg: str) -> Instruction | None:
+        msg = msg.split()
+        msg = [n.upper() for n in msg]
+        # print(msg)
+        if len(msg) != 2:
+            print(f"[INVALID COMMAND] Expected 2 words, but got {len(msg)}.")
+            self.send_message(AgvCommand.invalid.value)
+            return
+
+        if msg[0] not in self.valid_commands:
+            print(f'[INVALID COMMAND] command "{msg[0]}" is not valid.')
+            self.send_message(AgvCommand.invalid.value)
+            return
+
+        try:
+            int(msg[1])
+        except ValueError:
+            print(
+                f'[INVALID COMMAND] expected a number as second term, but got "{msg[1]}."'
+            )
+            self.send_message(AgvCommand.invalid.value)
+            return
+
+        # TODO: FInd the actual command corresponding with the msg
+        inst = Instruction(command=msg[0], value=msg[1])
+        self.send_message(AgvCommand.valid.value)
+
+        return inst
+
+    def instruction_handler(self):
+        while True:
+            self.mutex.acquire()
+            n = len(self.instructions)
+            if n > 0:
+                # process instruction then pop
+                pass
+
+            self.mutex.release()
+
+    def emergency_stop(self):
+        # stop everything, then clear instruction list.
+        return
 
 
 def main():
