1620ff4fb896d8baa33bf66df0b01b6a42ae19f4
Ali K <karimiafsharali@gmail.com>
2022-04-25 13:06:43 -0500

    correct pin assignment for each motor

--- agvtest.py
+++ agvtest.py
@@ -5,20 +5,23 @@ import gpiozero
 from tools.agv_tools import AgvTools
 
 direction_gpio_bcm = 18
-left_motor_gpio_bcm = 23
-right_motor_gpio_bcm = 24
+left_motor_gpio_bcm = 24
+right_motor_gpio_bcm = 23
 
 freq = AgvTools.calc_pulse_freq(velocity=2.25)
-print(freq)
+duty_cycle = 1
+print(f"frequency: {freq}\nduty cycle: {duty_cycle}")
 
 direction = gpiozero.OutputDevice(direction_gpio_bcm)
 left_motor = gpiozero.PWMOutputDevice(
-    left_motor_gpio_bcm, initial_value=0.5, frequency=freq
+    left_motor_gpio_bcm, initial_value=duty_cycle, frequency=freq
 )
 right_motor = gpiozero.PWMOutputDevice(
-    right_motor_gpio_bcm, initial_value=0.5, frequency=freq
+    right_motor_gpio_bcm, initial_value=duty_cycle, frequency=freq
 )
 
+direction.on()
 left_motor.blink()
+right_motor.blink()
 
 input()
