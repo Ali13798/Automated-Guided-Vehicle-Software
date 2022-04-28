5a1dabc789de56d4361082afac7f43febe3dc088
Ali K <karimiafsharali@gmail.com>
2022-04-28 18:35:56 -0500

    add pigpiod test

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 from time import sleep
 
-import RPi.GPIO as GPIO
-
 DIR = 18
 LEFT = 24
 RIGHT = 23
@@ -9,30 +7,60 @@ FORWARD = 0
 BACKWARD = 1
 SPR = 400
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(LEFT, GPIO.OUT)
-GPIO.setup(RIGHT, GPIO.OUT)
 
-GPIO.output(DIR, FORWARD)
+def test2():
+    import pigpiod
 
-step_count = SPR
-delay = 1 / SPR
+    pi = pigpiod.pi()
 
-for x in range(step_count):
-    GPIO.output(LEFT, GPIO.HIGH)
-    GPIO.output(RIGHT, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(LEFT, GPIO.LOW)
-    GPIO.output(RIGHT, GPIO.LOW)
-    sleep(delay)
+    pi.set_PWM_dutycycle(LEFT, 128)
+    pi.set_PWM_dutycycle(RIGHT, 128)
 
-sleep(0.5)
-GPIO.output(DIR, BACKWARD)
-for x in range(step_count):
-    GPIO.output(LEFT, GPIO.HIGH)
-    GPIO.output(RIGHT, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(LEFT, GPIO.LOW)
-    GPIO.output(RIGHT, GPIO.LOW)
-    sleep(delay)
+    pi.set_PWM_frequency(LEFT, 100)
+    pi.set_PWM_frequency(LEFT, 100)
+
+    try:
+        while True:
+            sleep(1)
+    except KeyboardInterrupt:
+        print("\nCtrl-C pressed. Stopping PIGPIO and exitting...")
+    finally:
+        pi.set_PWM_dutycycle(LEFT, 0)
+        pi.set_PWM_dutycycle(RIGHT, 0)
+        pi.stop()
+
+
+def test1():
+    import RPi.GPIO as GPIO
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(DIR, GPIO.OUT)
+    GPIO.setup(LEFT, GPIO.OUT)
+    GPIO.setup(RIGHT, GPIO.OUT)
+
+    GPIO.output(DIR, FORWARD)
+
+    step_count = SPR
+    delay = 1 / SPR
+
+    for x in range(step_count):
+        GPIO.output(LEFT, GPIO.HIGH)
+        GPIO.output(RIGHT, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(LEFT, GPIO.LOW)
+        GPIO.output(RIGHT, GPIO.LOW)
+        sleep(delay)
+
+    sleep(0.5)
+    GPIO.output(DIR, BACKWARD)
+    for x in range(step_count):
+        GPIO.output(LEFT, GPIO.HIGH)
+        GPIO.output(RIGHT, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(LEFT, GPIO.LOW)
+        GPIO.output(RIGHT, GPIO.LOW)
+        sleep(delay)
+
+
+if __name__ == "__main__":
+    test2()
