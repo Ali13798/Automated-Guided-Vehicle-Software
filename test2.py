df26dfc98f5c6be90f2c2f03fb14969e9bb5a2e7
Ali K <karimiafsharali@gmail.com>
2022-05-02 12:57:58 -0500

    experimental switch for rampup and rampdown

--- test2.py
+++ test2.py
@@ -19,24 +19,28 @@ rampdown = rampup.copy()
 rampdown.reverse()
 
 #! Experiment
-# SWITCH = 0
-# pi.set_mode(SWITCH, pigpio.INPUT)
+SWITCH = 12
+pi.set_mode(SWITCH, pigpio.INPUT)
 
 try:
     old_ramp = 0
     while True:
         #! Experiment
-        # new_ramp = pi.read(SWITCH)
-        new_ramp = input("Enter 0 for up or 1 for down: ")
+        new_ramp = pi.read(SWITCH)
+        # new_ramp = input("Enter 0 for up or 1 for down: ")
 
         if int(new_ramp) == old_ramp:
             sleep(0.5)
             continue
 
         if new_ramp:
-            AgvTools.generate_ramp(rampup)
+            AgvTools.generate_ramp(
+                pi=pi, motor_pin=MOTOR, ramp=rampup, clear_waves=True
+            )
         else:
-            AgvTools.generate_ramp(rampdown)
+            AgvTools.generate_ramp(
+                pi=pi, motor_pin=MOTOR, ramp=rampdown, clear_waves=True
+            )
         old_ramp = new_ramp
 
         print(f"Right freq={pi.get_PWM_frequency(MOTOR)}")
