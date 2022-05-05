478361e2613190e2c30bbbe949921320f7861f3b
Ali K <karimiafsharali@gmail.com>
2022-05-05 14:18:22 -0500

    adjust pin assignments and frequency of motors

--- agvtest.py
+++ agvtest.py
@@ -2,26 +2,27 @@ from time import sleep
 
 import gpiozero
 
+from onboard_controller.pi_bcm_pin_assignment import Pin
 from tools.agv_tools import AgvTools
 
-direction_gpio_bcm = 18
-left_motor_gpio_bcm = 24
-right_motor_gpio_bcm = 23
+left_direction_gpio_bcm = Pin.left_motor_backward_direction.value
+right_direction_gpio_bcm = Pin.right_motor_backward_direction.value
+motors_gpio_bcm = Pin.motors.value
 
 freq = AgvTools.calc_pulse_freq(velocity=2.25)
+freq = 20
 duty_cycle = 1
 print(f"frequency: {freq}\nduty cycle: {duty_cycle}")
 
-direction = gpiozero.OutputDevice(direction_gpio_bcm)
-left_motor = gpiozero.PWMOutputDevice(
-    left_motor_gpio_bcm, initial_value=duty_cycle, frequency=freq
-)
-right_motor = gpiozero.PWMOutputDevice(
-    right_motor_gpio_bcm, initial_value=duty_cycle, frequency=freq
+direction_left = gpiozero.OutputDevice(left_direction_gpio_bcm)
+direction_right = gpiozero.OutputDevice(right_direction_gpio_bcm)
+motors = gpiozero.PWMOutputDevice(
+    motors_gpio_bcm, initial_value=duty_cycle, frequency=freq
 )
 
-direction.on()
-left_motor.blink()
-right_motor.blink()
+direction_left.on()
+direction_right.on()
+
+motors.blink(on_time=1 / freq, off_time=1 / freq)
 
 input()
