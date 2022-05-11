4f45550d268df41391c27333b9748fb252928367
Ali K <karimiafsharali@gmail.com>
2022-05-11 11:21:45 -0500

    change pin assignment for left and right sides

--- onboard_controller/pi_bcm_pin_assignment.py
+++ onboard_controller/pi_bcm_pin_assignment.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class Pin(Enum):
     motors = 23
-    right_motor_backward_direction = 18
-    left_motor_backward_direction = 26
-    left_motor_kill_switch = 12
-    right_motor_kill_switch = 22
+    right_motor_backward_direction = 26
+    left_motor_backward_direction = 18
+    left_motor_kill_switch = 22
+    right_motor_kill_switch = 12
